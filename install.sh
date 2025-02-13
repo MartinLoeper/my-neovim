b65ae7a89e1c5c5c6510e0e7716329739576caf0
Martin Löper <martin.loeper@nesto-software.de>
2025-02-13 21:48:35 +0100

    fix: locale
    see:
    https://stackoverflow.com/questions/19305291/remnant-characters-when-tab-completing-with-zsh

--- install.sh
+++ install.sh
@@ -8,3 +8,5 @@ rm /tmp/devbox-installer.sh
 devbox global add home-manager
 refresh-global
 echo 'eval "$(devbox global shellenv --init-hook)"' >> ~/.zshrc
+echo "export LC_ALL=en_US.UTF-8" >> ~/.zshrc
+
