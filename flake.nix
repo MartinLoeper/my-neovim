2c569be5e56d0d18c01accfb42a523618f513419
Martin Löper <martin.loeper@nesto-software.de>
2025-02-20 09:37:09 +0100

    feat: add system-specific outputs

--- flake.nix
+++ flake.nix
@@ -4,13 +4,16 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { nixpkgs, home-manager, ... }: {
-    homeConfigurations."vscode" = home-manager.lib.homeManagerConfiguration {
-      pkgs = import nixpkgs { system = builtins.currentSystem; };
-      modules = [ ./default.nix ./devcontainer.nix ];
-    };
-    homeManagerModule = ./default.nix;
+  outputs = { nixpkgs, home-manager, flake-utils, ... }: {
+    homeConfigurations = flake-utils.lib.eachDefaultSystem (system:
+      home-manager.lib.homeManagerConfiguration {
+        pkgs = import nixpkgs { inherit system; };
+        modules = [ ./default.nix ./devcontainer.nix ];
+      });
+    homeManagerModule =
+      flake-utils.lib.eachDefaultSystem (system: ./default.nix);
   };
 }
