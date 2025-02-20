b95b03a4c5b97621ab7defc66d5e4bc444b83cdb
Martin Löper <martin.loeper@nesto-software.de>
2025-02-20 20:37:21 +0100

    feat: add bindings for harpoon files

--- default.nix
+++ default.nix
@@ -260,10 +260,10 @@
             vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end, { noremap = true, silent = true })
             vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { noremap = true, silent = true })
 
-            -- vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
-            -- vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
-            -- vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
-            -- vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
+            vim.keymap.set("n", "<Leader>j", function() harpoon:list():select(1) end)
+            vim.keymap.set("n", "<Leader>k", function() harpoon:list():select(2) end)
+            vim.keymap.set("n", "<Leader>l", function() harpoon:list():select(3) end)
+            vim.keymap.set("n", "<Leader>;", function() harpoon:list():select(4) end)
 
             -- Toggle previous & next buffers stored within Harpoon list
             vim.keymap.set("n", "<C-P>", function() harpoon:list():prev() end, { noremap = true, silent = true })
