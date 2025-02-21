cf011cbab97d004ada1677329c7d5eff5b1d3fe1
Martin Löper <martin.loeper@nesto-software.de>
2025-02-22 00:16:44 +0100

    fix: only modify copy

--- default.nix
+++ default.nix
@@ -78,23 +78,12 @@
       extraLuaConfig = ''
               vim.keymap.set("n", "<Space>", "<Nop>", { silent = true, remap = false })
               vim.g.mapleader = " "
-              
-              local function paste()
-                return {
-                  vim.fn.split(vim.fn.getreg(""), "\n"),
-                  vim.fn.getregtype(""),
-                }
-              end
 
               vim.g.clipboard = {
                 name = "OSC 52",
                 copy = {
                   ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
                   ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
-                },
-                paste = {
-                  ["+"] = paste,
-                  ["*"] = paste,
                 },
               }
                                                                                             
