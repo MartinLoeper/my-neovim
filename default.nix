cad37b06eddac5eb495fbcaf5cfa120c7d8574bf
Martin Löper <martin.loeper@nesto-software.de>
2025-02-24 00:08:08 +0100

    feat: switch to catppuccin theme

--- default.nix
+++ default.nix
@@ -79,8 +79,6 @@
               vim.keymap.set("n", "<Space>", "<Nop>", { silent = true, remap = false })
               vim.g.mapleader = " "
 
-              vim.cmd.colorscheme("colorbuddy")
-
               vim.o.clipboard = "unnamedplus"
               local function paste()
                 return {
@@ -274,7 +272,63 @@
         pkgs.vimPlugins.cmp-buffer
         pkgs.vimPlugins.lualine-lsp-progress
         pkgs.vimPlugins.which-key-nvim
-        pkgs.vimPlugins.colorbuddy-nvim
+        {
+          plugin = pkgs.vimPlugins.catppuccin-nvim;
+          config = ''
+            require("catppuccin").setup({
+              flavour = "mocha", -- latte, frappe, macchiato, mocha
+              background = { -- :h background
+                  light = "latte",
+                  dark = "mocha",
+              },
+              transparent_background = false, -- disables setting the background color.
+              show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
+              term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+              dim_inactive = {
+                  enabled = false, -- dims the background color of inactive window
+                  shade = "dark",
+                  percentage = 0.15, -- percentage of the shade to apply to the inactive window
+              },
+              no_italic = false, -- Force no italic
+              no_bold = false, -- Force no bold
+              no_underline = false, -- Force no underline
+              styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+                  comments = { "italic" }, -- Change the style of comments
+                  conditionals = { "italic" },
+                  loops = {},
+                  functions = {},
+                  keywords = {},
+                  strings = {},
+                  variables = {},
+                  numbers = {},
+                  booleans = {},
+                  properties = {},
+                  types = {},
+                  operators = {},
+                  -- miscs = {}, -- Uncomment to turn off hard-coded styles
+              },
+              color_overrides = {},
+              custom_highlights = {},
+              default_integrations = true,
+              integrations = {
+                  cmp = true,
+                  gitsigns = true,
+                  nvimtree = true,
+                  treesitter = true,
+                  notify = false,
+                  mini = {
+                      enabled = true,
+                      indentscope_color = "",
+                  },
+                  -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+              },
+            })
+
+            -- setup must be called before loading
+            vim.cmd.colorscheme "catppuccin"
+          '';
+          type = "lua";
+        }
         {
           plugin = pkgs.vimPlugins.gitsigns-nvim;
           config = ''
@@ -382,6 +436,7 @@
           config = ''
             vim.opt.termguicolors = true
             require("bufferline").setup{
+              highlights = require("catppuccin.groups.integrations.bufferline").get(),
               options = {
                 diagnostics = "nvim_lsp",
                 offsets = {
