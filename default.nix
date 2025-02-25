317966a3b09b4c94b98ef1577844d7a4df9aa5c1
Martin Löper <martin.loeper@nesto-software.de>
2025-02-25 18:28:23 +0100

    fix: minimap autostart

--- default.nix
+++ default.nix
@@ -619,6 +619,7 @@
         {
           plugin = pkgs.vimPlugins.minimap-vim;
           config = ''
+            vim.g.minimap_auto_start = 1
             vim.g.minimap_auto_start_win_enter = 1
           '';
           type = "lua";
