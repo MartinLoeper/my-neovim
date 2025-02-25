80fab30340b9d80b80db23e74eeb59615a04a126
Martin Löper <martin.loeper@nesto-software.de>
2025-02-25 22:01:35 +0100

    fix: sha256 for neominimap

--- plugins/neominimap.nix
+++ plugins/neominimap.nix
@@ -7,7 +7,7 @@ let
       owner = "Isrothy";
       repo = "neominimap.nvim";
       rev = "505e756fc96d05a7c372792fe76e346aa0ed9240";
-      hash = "sha256-udElcii8uK8YKg54srBHvdZCNH80rsB5VdPTyoWchSE=";
+      hash = "sha256-HQEgVk3xdIihg0kVV83PikOo008DblDhxGGswKryvMo=";
     };
     rockspecVersion = "1.1";
     rocksSubdir = "dummy";
