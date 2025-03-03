4e0aefc2be83c0cdce694ac6e4b04cf75d88d42e
Martin Löper <martin.loeper@nesto-software.de>
2025-03-03 23:30:55 +0100

    remove the custom clipboard logic

--- default.nix
+++ default.nix
@@ -79,26 +79,6 @@
         vim.keymap.set("n", "<Space>", "<Nop>", { silent = true, remap = false })
         vim.g.mapleader = " "
 
-        vim.o.clipboard = "unnamedplus"
-        local function paste()
-          return {
-            vim.fn.split(vim.fn.getreg(""), "\n"),
-            vim.fn.getregtype(""),
-          }
-        end
-
-        vim.g.clipboard = {
-          name = "OSC 52",
-          copy = {
-            ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
-            ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
-          },
-          paste = {
-            ["+"] = paste,
-            ["*"] = paste,
-          },
-        }
-
         -- more space for the line numbers
         vim.opt.numberwidth = 6
 
