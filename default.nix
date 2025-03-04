e0d0508e6b8b7e48694afb723820c6cd8ef097a7
Martin Löper <martin.loeper@nesto-software.de>
2025-03-04 11:42:30 +0100

    feat: add noice

--- default.nix
+++ default.nix
@@ -795,6 +795,7 @@
         (import ./plugins/leetcode.nix { inherit pkgs; })
         (import ./plugins/lazygit.nix { inherit pkgs; })
         (import ./plugins/comment.nix { inherit pkgs; })
+        (import ./plugins/noice.nix { inherit pkgs; })
         pkgs.vimPlugins.nui-nvim # leetcode dep
         pkgs.vimPlugins.plenary-nvim # leetcode dep
         {
