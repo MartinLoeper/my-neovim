83bae9b274591f30c372bef2ef7d2722201d0c04
Martin Löper <martin.loeper@nesto-software.de>
2025-03-08 21:38:56 +0100

    feat: add anthropic adapter and make default for chat and inline

--- plugins/codecompanion.nix
+++ plugins/codecompanion.nix
@@ -2,7 +2,22 @@
   plugin = pkgs.vimPlugins.codecompanion-nvim;
   config = ''
     require("codecompanion").setup({
+      strategies = {
+        chat = {
+          adapter = "anthropic",
+        },
+        inline = {
+          adapter = "anthropic",
+        },
+       },
        adapters = {
+         anthropic = function()
+          return require("codecompanion.adapters").extend("anthropic", {
+            env = {
+              api_key = "cmd: cat /home/mloeper/.config/sops-nix/secrets/anthropic-api-key"
+            },
+          })
+         end,
          openai = function()
            return require("codecompanion.adapters").extend("openai", {
              env = {
