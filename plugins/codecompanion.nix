cff8767a2dd6770535bbd62e1aed25d54ac65ebb
Martin Löper <martin.loeper@nesto-software.de>
2025-03-08 22:21:16 +0100

    feat: add shortcuts for codecompanion

--- plugins/codecompanion.nix
+++ plugins/codecompanion.nix
@@ -27,6 +27,11 @@
          end,
        },
     })
+
+    vim.keymap.set({ "n", "v" }, "<C-a>", "<cmd>CodeCompanionActions<cr>", { noremap = true, silent = true })
+    vim.keymap.set({ "n", "v" }, "<LocalLeader>a", "<cmd>CodeCompanionChat Toggle<cr>", { noremap = true, silent = true })
+    vim.keymap.set("v", "ga", "<cmd>CodeCompanionChat Add<cr>", { noremap = true, silent = true })
+    vim.cmd([[cab cc CodeCompanion]])
   '';
   type = "lua";
 }
