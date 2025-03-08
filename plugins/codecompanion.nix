9db670f25beef17f184746d1b36e95f13c66fc6f
Martin Löper <martin.loeper@nesto-software.de>
2025-03-08 20:50:06 +0100

    feat: add codecompanion with openapi integration

--- plugins/codecompanion.nix
+++ plugins/codecompanion.nix
@@ -1,7 +1,17 @@
 { pkgs, ... }: {
   plugin = pkgs.vimPlugins.codecompanion-nvim;
   config = ''
-    require("codecompanion").setup()
+    require("codecompanion").setup({
+       adapters = {
+         openai = function()
+           return require("codecompanion.adapters").extend("openai", {
+             env = {
+               api_key = "cmd: cat /home/mloeper/.config/sops-nix/secrets/openai-api-key"
+             },
+           })
+         end,
+       },
+    })
   '';
   type = "lua";
 }
