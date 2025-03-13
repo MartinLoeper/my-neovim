f0bbc86c76e0b37f041f5e7f8f99241a8b58dd7e
Martin Löper <martin.loeper@nesto-software.de>
2025-03-14 00:08:44 +0100

    fix: sri hash

--- plugins/beacon.nix
+++ plugins/beacon.nix
@@ -7,7 +7,7 @@ let
       owner = "DanilaMihailov";
       repo = "beacon.nvim";
       rev = "098ff96c33874339d5e61656f3050dbd587d6bd5";
-      hash = "sha256-0di4z5xkdyaqpmaa1iwirys91ikjm05g3pxr6sn3zh9h36czvzn7";
+      hash = "sha256-x/79mRkwwT+sNrnf8QqocsaQtM+Rx6BUvVj5Nnv5JDY=";
     };
     rockspecVersion = "1.1";
     rocksSubdir = "dummy";
