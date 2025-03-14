3a6952f7ddbdc92d249af8848b9f9fa6cc680073
Martin Löper <martin.loeper@nesto-software.de>
2025-03-14 18:50:40 +0100

    fix: syntax issue in lua

--- plugins/codecompanion.nix
+++ plugins/codecompanion.nix
@@ -1,6 +1,5 @@
 { pkgs, ... }: {
   plugin = pkgs.vimPlugins.codecompanion-nvim;
-  config = builtins.readFile ./codecompanion.lua + "\n"
-    + builtins.readFile ./codecompanion-fidget.lua;
+  config = builtins.readFile ./codecompanion.lua;
   type = "lua";
 }
