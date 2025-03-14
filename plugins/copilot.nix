de2f86b3e2d179b467aab772c5144f997fe563fe
Martin Löper <martin.loeper@nesto-software.de>
2025-03-14 18:49:11 +0100

    feat: add option to toggle copilot suggestion

--- plugins/copilot.nix
+++ plugins/copilot.nix
@@ -1,21 +1,27 @@
 { pkgs, ... }: {
   plugin = pkgs.vimPlugins.copilot-lua;
   config = ''
-    require("copilot").setup({
-      suggestion = { 
-        enabled = true, 
-        auto_trigger = true,
-        keymap = {
-          accept = "<S-Tab>",
-          accept_word = "<M-Tab>",
-        },
-      },
-      panel = { enabled = false, auto_refresh = true },
+     require("copilot").setup({
+       suggestion = { 
+         enabled = true, 
+         auto_trigger = true,
+         keymap = {
+           accept = "<S-Tab>",
+           accept_word = "<M-Tab>",
+         },
+       },
+       panel = { enabled = false, auto_refresh = true },
     })
 
-    -- make the suggestions italic s.t. the monaspace handwritten font is used
-    -- see pending issue: https://github.com/zbirenbaum/copilot.lua/issues/324
-    vim.api.nvim_set_hl(0, "CopilotSuggestion", { fg = "#555555", italic = true })
+     vim.keymap.set('n', '<leader>ts', function()
+       local enabled = vim.g.copilot_enabled or true
+       vim.g.copilot_enabled = not enabled
+       print("Copilot " .. (not enabled and "enabled" or "disabled"))
+     end, { desc = "Toggle Copilot" })
+
+     -- make the suggestions italic s.t. the monaspace handwritten font is used
+     -- see pending issue: https://github.com/zbirenbaum/copilot.lua/issues/324
+     vim.api.nvim_set_hl(0, "CopilotSuggestion", { fg = "#555555", italic = true })
   '';
   type = "lua";
 }
