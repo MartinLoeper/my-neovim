7dbd589275b7f40f5132a29ab6366407121e2f0e
Martin Löper <martin.loeper@nesto-software.de>
2025-03-14 19:03:02 +0100

    fix: try to fix the copilot switch

--- plugins/copilot.nix
+++ plugins/copilot.nix
@@ -13,11 +13,31 @@
        panel = { enabled = false, auto_refresh = true },
     })
 
-     vim.keymap.set('n', '<leader>ts', function()
-       local enabled = vim.g.copilot_enabled or true
-       vim.g.copilot_enabled = not enabled
-       print("Copilot " .. (not enabled and "enabled" or "disabled"))
-     end, { desc = "Toggle Copilot" })
+    local toggleCopilot = function()
+      local copilot = require("copilot")
+      local client = require("copilot.client")
+      -- toggle global variable for stop condition
+      -- first toggle sets the none existing variable to true
+      vim.g.copilot_enabled = not vim.g.copilot_enabled
+      -- stop or start copilot
+      local noti = require("notify")
+      local noti_opts = { title = "Copilot", icon = "", timeout = 1000, hide_from_history = true }
+      if vim.g.copilot_enabled then
+        -- spin up lsp from scratch ang get client setup and attached
+        copilot.setup()
+        client.setup()
+        noti("ON", "info", noti_opts)
+      else
+        -- detatch first to prevent lsp spamming it's own notifications when teardown is called
+        client.buf_detach()
+        -- destroy microsoft XD
+        client.teardown()
+        noti("OFF", "error", noti_opts)
+      end
+    end
+
+    api.nvim_create_user_command("ToggleCopilot", toggleCopilot, { range = false })
+    vim.keymap.set("n", "<leader>ts", "<cmd>ToggleCopilot<cr>", { desc = " Toggle Copilot" })
 
      -- make the suggestions italic s.t. the monaspace handwritten font is used
      -- see pending issue: https://github.com/zbirenbaum/copilot.lua/issues/324
