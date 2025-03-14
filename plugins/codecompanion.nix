3450267c7bd27968f658680ad5e5b9037a9fdbb7
Martin Löper <martin.loeper@nesto-software.de>
2025-03-14 23:35:00 +0100

    fix: concatStrings import

--- plugins/codecompanion.nix
+++ plugins/codecompanion.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }: {
   plugin = pkgs.vimPlugins.codecompanion-nvim;
-  config = builtins.concatStrings [
+  config = pkgs.lib.strings.concatStrings [
     (builtins.readFile ./codecompanion.lua)
     (builtins.readFile ./codecompanion-fidget.lua)
   ];
