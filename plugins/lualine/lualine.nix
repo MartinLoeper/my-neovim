6133f58c15686fd24cbc05b9c879bde1a74a1af7
Martin Löper <martin.loeper@nesto-software.de>
2025-03-15 03:00:52 +0100

    fix: lualine codecompanion

--- plugins/lualine/lualine.nix
+++ plugins/lualine/lualine.nix
@@ -1,8 +1,6 @@
 { pkgs, ... }: {
   plugin = pkgs.vimPlugins.lualine-nvim;
   config = ''
-    local codecompanion = require('lualine-codecompanion')
-
     require('lualine').setup {
       sections = {
         lualine_f = { 'filename' },
@@ -10,6 +8,27 @@
           'lsp_progress'
         },
         lualine_x = { 'copilot' ,'encoding', 'fileformat', 'filetype' },
+      },
+      extensions = {
+        {
+          filetypes = { "codecompanion" },
+          sections = {
+            lualine_a = {
+            },
+            lualine_b = {
+              require('lualine-codecompanion')
+            },
+            lualine_c = {
+            },
+            lualine_x = {},
+            lualine_y = {
+              "progress",
+            },
+            lualine_z = {
+              "location",
+            },
+          },
+        },
       },
     }
   '';
