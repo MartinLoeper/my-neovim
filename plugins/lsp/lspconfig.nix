e24181b4fa0cae944f861fb24b5c88f3f33c3d92
Martin Löper <martin.loeper@nesto-software.de>
2025-03-17 22:54:21 +0100

    debug

--- plugins/lsp/lspconfig.nix
+++ plugins/lsp/lspconfig.nix
@@ -51,14 +51,6 @@
         Lua = {}
       }
     }
-
-    -- configure floating preview for lsp
-    local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
-    function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
-      opts = opts or {}
-      opts.border = opts.border or border
-      return orig_util_open_floating_preview(contents, syntax, opts, ...)
-    end
   '';
   type = "lua";
 }
