4d9f6fc847ce3f2e91333576028934549aa5d335
Martin Löper <martin.loeper@nesto-software.de>
2025-03-17 22:26:57 +0100

    feat: enable cursorline by default

--- default.nix
+++ default.nix
@@ -259,6 +259,8 @@
           vim.o.shiftwidth = 2
           vim.o.expandtab = true  -- Converts tabs to spaces
         end
+
+        vim.opt.cursorline = true
       '';
       plugins = [
         (import ./plugins/nvim-tree/nvim-tree.nix { inherit pkgs; })
@@ -310,80 +312,7 @@
         (import ./plugins/treesitter-textsubjects.nix { inherit pkgs; })
         (import ./plugins/toggleterm.nix { inherit pkgs; })
         (import ./plugins/competitest.nix { inherit pkgs; })
-        {
-          plugin = pkgs.vimPlugins.gitsigns-nvim;
-          config = ''
-            require('gitsigns').setup{
-              current_line_blame = true,
-              numhl = true,
-              on_attach = function(bufnr)
-                local gitsigns = require('gitsigns')
-
-                local function map(mode, l, r, opts)
-                  opts = opts or {}
-                  opts.buffer = bufnr
-                  vim.keymap.set(mode, l, r, opts)
-                end
-
-                -- Navigation
-                map('n', ']c', function()
-                  if vim.wo.diff then
-                    vim.cmd.normal({']c', bang = true})
-                  else
-                    gitsigns.nav_hunk('next')
-                  end
-                end)
-
-                map('n', '[c', function()
-                  if vim.wo.diff then
-                    vim.cmd.normal({'[c', bang = true})
-                  else
-                    gitsigns.nav_hunk('prev')
-                  end
-                end)
-
-                -- Actions
-                map('n', '<leader>hs', gitsigns.stage_hunk)
-                map('n', '<leader>hr', gitsigns.reset_hunk)
-
-                map('v', '<leader>hs', function()
-                  gitsigns.stage_hunk({ vim.fn.line('.'), vim.fn.line('v') })
-                end)
-
-                map('v', '<leader>hr', function()
-                  gitsigns.reset_hunk({ vim.fn.line('.'), vim.fn.line('v') })
-                end)
-
-                map('n', '<leader>hS', gitsigns.stage_buffer)
-                map('n', '<leader>hR', gitsigns.reset_buffer)
-                map('n', '<leader>hp', gitsigns.preview_hunk)
-                map('n', '<leader>hi', gitsigns.preview_hunk_inline)
-
-                map('n', '<leader>hb', function()
-                  gitsigns.blame_line({ full = true })
-                end)
-
-                map('n', '<leader>hd', gitsigns.diffthis)
-
-                map('n', '<leader>hD', function()
-                  gitsigns.diffthis('~')
-                end)
-
-                map('n', '<leader>hQ', function() gitsigns.setqflist('all') end)
-                map('n', '<leader>hq', gitsigns.setqflist)
-
-                -- Toggles
-                map('n', '<leader>tb', gitsigns.toggle_current_line_blame)
-                map('n', '<leader>td', gitsigns.toggle_deleted)
-                map('n', '<leader>tw', gitsigns.toggle_word_diff)
-
-                -- Text object
-                map({'o', 'x'}, 'ih', gitsigns.select_hunk)
-              end
-            }
-          '';
-          type = "lua";
-        }
+        (import ./plugins/git/gitsigns.nix { inherit pkgs; })
         pkgs.vimPlugins.yazi-nvim
         pkgs.vimPlugins.nvim-notify
         {
