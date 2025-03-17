4600a2b93890e4388460dd3447158a93f0ac5433
Martin Löper <martin.loeper@nesto-software.de>
2025-03-18 00:37:06 +0100

    fix

--- plugins/cmp/friendly-snippets.nix
+++ plugins/cmp/friendly-snippets.nix
@@ -1,10 +1,1 @@
-{ pkgs, ... }: {
-  plugin = pkgs.vimPlugins.friendly-snippets;
-  config = ''
-    vim.tbl_map(
-      function(type) require("luasnip.loaders.from_" .. type).lazy_load() end,
-      { "vscode", "snipmate", "lua" }
-    )
-  '';
-  type = "lua";
-}
+{ pkgs, ... }: { plugin = pkgs.vimPlugins.friendly-snippets; }
