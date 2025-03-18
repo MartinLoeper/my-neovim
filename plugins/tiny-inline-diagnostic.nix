0fcdbd4888c2876ad17f6f1ff113ced6cb491ce2
Martin Löper <martin.loeper@nesto-software.de>
2025-03-18 23:59:50 +0100

    feat: change preset to powerline

--- plugins/tiny-inline-diagnostic.nix
+++ plugins/tiny-inline-diagnostic.nix
@@ -1,7 +1,9 @@
 { pkgs, ... }: {
   plugin = pkgs.vimPlugins.tiny-inline-diagnostic-nvim;
   config = ''
-    require("tiny-inline-diagnostic").setup()
+    require("tiny-inline-diagnostic").setup({
+      preset = "powerline"
+    })
 
     vim.diagnostic.config({ virtual_text = false })
   '';
