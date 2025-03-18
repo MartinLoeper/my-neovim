faf4b4f6ec9f83b2e90d022f3d15d549f764fd6b
Martin Löper <martin.loeper@nesto-software.de>
2025-03-18 12:42:55 +0100

    fix

--- plugins/outline.nix
+++ plugins/outline.nix
@@ -1,7 +1,12 @@
 { pkgs, ... }: {
   plugin = pkgs.vimPlugins.outline-nvim;
   config = ''
-    require("outline").setup({})
+    require("outline").setup({
+      outline_window = {
+        auto_jump = true,
+        wrap = true,
+      }
+    })
 
     vim.keymap.set('n', '<leader>n', '<cmd>Outline<CR>', { noremap = true, silent = true })
   '';
