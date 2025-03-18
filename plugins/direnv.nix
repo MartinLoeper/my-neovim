62acb25c8763b846a8001e7783dea9cbb74ae91f
Martin Löper <martin.loeper@nesto-software.de>
2025-03-18 17:38:37 +0100

    fix

--- plugins/direnv.nix
+++ plugins/direnv.nix
@@ -7,7 +7,7 @@ let
       owner = "NotAShelf";
       repo = "direnv.nvim";
       rev = "3e38d855c764bb1bec230130ed0e026fca54e4c8";
-      hash = "sha256-x/79mRkwwT+sNrnf8QqocsaQtM+Rx6BevVj5Nnv5JDY=";
+      hash = "sha256-nWdAIchqGsWiF0cQ7NwePRa1fpugE8duZKqdBaisrAc=";
     };
     rockspecVersion = "1.1";
     rocksSubdir = "dummy";
