01665da97b331a3a877227653adbed2616f00452
Martin Löper <martin.loeper@nesto-software.de>
2025-03-18 18:19:27 +0100

    feat: add trouble

--- default.nix
+++ default.nix
@@ -170,6 +170,7 @@
         (import ./plugins/startify.nix { inherit pkgs; })
         (import ./plugins/cmp/friendly-snippets.nix { inherit pkgs; })
         (import ./plugins/overseer.nix { inherit pkgs; })
+        (import ./plugins/trouble.nix { inherit pkgs; })
         (import ./plugins/direnv.nix {
           lua = pkgs.lua;
           fetchFromGitHub = pkgs.fetchFromGitHub;
