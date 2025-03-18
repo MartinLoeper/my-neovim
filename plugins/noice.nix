b75e85bdd441d31cfc5d5c85c1e8fcbfb7568db3
Martin Löper <martin.loeper@nesto-software.de>
2025-03-18 14:29:13 +0100

    fix: disable noice message handling

--- plugins/noice.nix
+++ plugins/noice.nix
@@ -3,12 +3,16 @@
   config = ''
     require("noice").setup({
       lsp = {
-        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-        override = {
-          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-          ["vim.lsp.util.stylize_markdown"] = true,
-          ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+        progress = {
+          enabled = false,
         },
+        signature = {
+          enabled = false,
+        }
+      },
+
+      messages = {
+        enabled = false,
       },
 
       -- you can enable a preset for easier configuration
