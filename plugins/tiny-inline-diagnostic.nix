14581dd158149732ee164e8068b9691da617e2f4
Martin Löper <martin.loeper@nesto-software.de>
2025-03-18 23:58:40 +0100

    fix

--- plugins/tiny-inline-diagnostic.nix
+++ plugins/tiny-inline-diagnostic.nix
@@ -2,6 +2,8 @@
   plugin = pkgs.vimPlugins.tiny-inline-diagnostic-nvim;
   config = ''
     require("tiny-inline-diagnostic").setup()
+
+    vim.diagnostic.config({ virtual_text = false })
   '';
   type = "lua";
 }
