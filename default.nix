f92708c42e8adc23864036f606245890fc301b4a
Martin Löper <martin.loeper@nesto-software.de>
2025-03-18 13:19:59 +0100

    fix: relative line number in lua

--- default.nix
+++ default.nix
@@ -50,10 +50,8 @@
     neovim = {
       enable = true;
       defaultEditor = true;
-      extraConfig = ''
-        set number relativenumber
-      '';
       extraLuaConfig = ''
+        vim.opt.relativenumber = true
         vim.keymap.set("n", "<Space>", "<Nop>", { silent = true, remap = false })
         vim.g.mapleader = " "
 
@@ -62,16 +60,6 @@
 
         -- more space for the line numbers
         vim.opt.numberwidth = 6
-
-        -- zoom in neovide when in neovide
-        if vim.g.neovide == true then
-          vim.api.nvim_set_keymap("n", "<C-=>", ":lua vim.g.neovide_scale_factor = math.min(vim.g.neovide_scale_factor + 0.1,  2.0)<CR>", { silent = true })
-          vim.api.nvim_set_keymap("n", "<C-->", ":lua vim.g.neovide_scale_factor = math.max(vim.g.neovide_scale_factor - 0.1,  0.1)<CR>", { silent = true })
-          vim.api.nvim_set_keymap("n", "<C-+>", ":lua vim.g.neovide_transparency = math.min(vim.g.neovide_transparency + 0.05, 1.0)<CR>", { silent = true })
-          vim.api.nvim_set_keymap("n", "<C-_>", ":lua vim.g.neovide_transparency = math.max(vim.g.neovide_transparency - 0.05, 0.0)<CR>", { silent = true })
-          vim.api.nvim_set_keymap("n", "<C-0>", ":lua vim.g.neovide_scale_factor = 0.5<CR>", { silent = true })
-          vim.api.nvim_set_keymap("n", "<C-)>", ":lua vim.g.neovide_transparency = 0.9<CR>", { silent = true })
-        end
 
         vim.keymap.set('n', '<leader>w', vim.cmd.write, { noremap = true, silent = true })
         vim.api.nvim_set_keymap('n', '<leader>q', ':q<CR>', { noremap = true, silent = true })
@@ -116,7 +104,6 @@
 
         vim.opt.smartindent = true
         vim.opt.autoindent = true
-
 
         vim.opt.foldmethod = "indent"  -- Use indentation levels for folding
         vim.opt.foldlevel = 99         -- Start with all folds open
