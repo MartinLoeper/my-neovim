ea3a26ddc6abd74bd0ee4e75dbe64a75ca76f38a
Martin Löper <martin.loeper@nesto-software.de>
2025-03-18 15:12:43 +0100

    fix

--- plugins/fidget.nix
+++ plugins/fidget.nix
@@ -59,7 +59,6 @@
     -- hide the cmd bar at the bottom
     -- noice does the same when it is in charge for handling messages
     vim.opt.cmdheight = 0
-    vim.opt.laststatus = 0
   '';
   type = "lua";
 }
