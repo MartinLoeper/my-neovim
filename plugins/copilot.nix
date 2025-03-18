4b99116fa11ab115fb0e184b8e88217b76a02813
Martin Löper <martin.loeper@nesto-software.de>
2025-03-18 14:57:06 +0100

    fix

--- plugins/copilot.nix
+++ plugins/copilot.nix
@@ -2,15 +2,6 @@
   plugin = pkgs.vimPlugins.copilot-lua;
   config = ''
      require("copilot").setup({
-       suggestion = {
-         enabled = false,
-         auto_trigger = true,
-         keymap = {
-           accept = "<C-Tab>",
-           accept_word = "<M-Tab>",
-         },
-       },
-       panel = { enabled = false, auto_refresh = true },
     })
 
     vim.g.copilot_enabled = false
@@ -26,7 +17,17 @@
       local noti_opts = { title = "Copilot", icon = "", timeout = 1000, hide_from_history = true }
       if vim.g.copilot_enabled then
         -- spin up lsp from scratch ang get client setup and attached
-        copilot.setup()
+        copilot.setup({
+          suggestion = {
+            enabled = true,
+            auto_trigger = true,
+            keymap = {
+              accept = "<C-Tab>",
+              accept_word = "<M-Tab>",
+            },
+          },
+          panel = { enabled = false, auto_refresh = true },
+        })
         client.setup()
         fidget.notify("Copilot ON", vim.log.levels.INFO)
       else
