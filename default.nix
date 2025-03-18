15bc0e71d0a6b3523d3130c306907219ee7a03db
Martin Löper <martin.loeper@nesto-software.de>
2025-03-18 12:56:30 +0100

    refactor: cmp

--- default.nix
+++ default.nix
@@ -117,114 +117,6 @@
         vim.opt.smartindent = true
         vim.opt.autoindent = true
 
-        local cmp = require'cmp'
-        local lspkind = require('lspkind')
-
-        -- add copilot symbol
-        local lspkind = require("lspkind")
-        lspkind.init({
-          symbol_map = {
-            Copilot = "",
-          },
-        })
-        vim.api.nvim_set_hl(0, "CmpItemKindCopilot", {fg ="#6CC644"})
-
-        local function replace_keys(str)
-          return vim.api.nvim_replace_termcodes(str, true, true, true)
-        end
-
-        local types = require('cmp.types')
-
-        cmp.setup({
-          completion = {
-            autocomplete = {
-              types.cmp.TriggerEvent.TextChanged,
-            },
-          },
-          formatting = {
-            fields = { "kind", "abbr", "menu" },
-            format = function(entry, vim_item)
-              local kind = require("lspkind").cmp_format({ mode = "symbol_text", maxwidth = 50 })(entry, vim_item)
-              local strings = vim.split(kind.kind, "%s", { trimempty = true })
-              kind.kind = " " .. (strings[1] or "") .. " "
-              kind.menu = "    (" .. (strings[2] or "") .. ")"
-
-              return kind
-            end,
-          },
-          mapping = {
-            ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-            ['<C-f>'] = cmp.mapping.scroll_docs(4),
-            ['<C-Space>'] = cmp.mapping.complete(),
-            ['<C-e>'] = cmp.mapping.abort(),
-            ['<CR>'] = cmp.mapping.confirm({ select = false, behavior = cmp.ConfirmBehavior.Replace }),
-              ['<Tab>'] = cmp.mapping(function(fallback)
-                if cmp.visible() then
-                  cmp.select_next_item()
-                elseif vim.fn['vsnip#available'](1) == 1 then
-                  vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>(vsnip-expand-or-jump)', true, true, true), "")
-                else
-                  fallback()
-                end
-              end, { 'i', 's' }),
-
-            -- TODO: fix the mapping, see above as reference
-            ['<C-j>'] = cmp.mapping(function(fallback)
-              if vim.call('vsnip#available', 1) ~= 0 then
-                vim.fn.feedkeys(replace_keys('<Plug>(vsnip-jump-next)'), ''')
-              elseif cmp.visible() then
-                cmp.select_next_item()
-              else
-                fallback()
-              end
-            end, { 'i', 's' }),
-            ['<C-k>'] = cmp.mapping(function(fallback)
-              if vim.call('vsnip#available', -1) ~= 0 then
-                vim.fn.feedkeys(replace_keys('<Plug>(vsnip-jump-prev)'), ''')
-              elseif cmp.visible() then
-                cmp.select_prev_item()
-              else
-                fallback()
-              end
-            end, { 'i', 's' }),
-          },
-          snippet = {
-            -- REQUIRED - you must specify a snippet engine
-            expand = function(args)
-              vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-            end,
-          },
-          window = {
-            -- completion = cmp.config.window.bordered(),
-            -- documentation = cmp.config.window.bordered(),
-          },
-          sources = cmp.config.sources({
-            { name = 'nvim_lsp' },
-            { name = 'vsnip' },
-            { name = 'path' },
-          }, {
-            { name = 'buffer' },
-          })
-        })
-
-        -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-        cmp.setup.cmdline({ '/', '?' }, {
-          mapping = cmp.mapping.preset.cmdline(),
-          sources = {
-            { name = 'buffer' }
-          }
-        })
-
-        -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-        cmp.setup.cmdline(':', {
-          mapping = cmp.mapping.preset.cmdline(),
-          sources = cmp.config.sources({
-            { name = 'path' },
-            { name = 'cmdline' },
-            { name = 'cmdline_history' }
-          }),
-          matching = { disallow_symbol_nonprefix_matching = false }
-        })
 
         vim.opt.foldmethod = "indent"  -- Use indentation levels for folding
         vim.opt.foldlevel = 99         -- Start with all folds open
@@ -312,7 +204,7 @@
         { plugin = pkgs.vimPlugins.cmp-vsnip; }
         pkgs.vimPlugins.vim-vsnip
         pkgs.vimPlugins.vim-vsnip-integ
-        { plugin = pkgs.vimPlugins.nvim-cmp; }
+        (import ./plugins/cmp/cmp.nix { inherit pkgs; })
         (import ./plugins/outline.nix { inherit pkgs; })
         (import ./plugins/lualine/lualine.nix { inherit pkgs; })
         (import ./plugins/lazygit.nix { inherit pkgs; })
