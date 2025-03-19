b2494d7eb61c5dbb81fe793c5afbbfd9528baabe
Martin Löper <martin.loeper@nesto-software.de>
2025-03-20 00:00:50 +0100

    fix: show diagnostics without hover

--- plugins/tiny-inline-diagnostic.nix
+++ plugins/tiny-inline-diagnostic.nix
@@ -2,10 +2,17 @@
   plugin = pkgs.vimPlugins.tiny-inline-diagnostic-nvim;
   config = ''
     require("tiny-inline-diagnostic").setup({
-      preset = "powerline"
+      preset = "powerline",
+      show_diagnostic_on_hover = false,  -- Don't wait for hover
+      diagnostic_delay = 0               -- No delay in showing diagnostics
     })
 
-    vim.diagnostic.config({ virtual_text = false })
+    vim.diagnostic.config({
+      virtual_text = false,  -- Disable default virtual text
+      underline = true,      -- Keep underlines
+      signs = true           -- Keep signs in the gutter
+    })
   '';
   type = "lua";
 }
+
