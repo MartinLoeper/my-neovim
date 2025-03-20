f586e78297592185ac072565729cda910202da3f
Martin Löper <martin.loeper@nesto-software.de>
2025-03-20 12:48:44 +0100

    fix

--- plugins/git-worktree.nix
+++ plugins/git-worktree.nix
@@ -1,5 +1,5 @@
 { pkgs, ... }: {
-  # WARN: primagen apparently abandoned his plugin, so we use a fork
+  # INFO: primagen apparently abandoned his plugin, so we use a fork
   plugin = (pkgs.vimPlugins.git-worktree-nvim.overrideAttrs (oldAttrs: {
     src = pkgs.fetchFromGitHub {
       owner = "polarmutex";
@@ -9,7 +9,18 @@
     };
   }));
   config = ''
-    require("git-worktree").setup()
+    local Hooks = require("git-worktree.hooks")
+    local config = require('git-worktree.config')
+    local update_on_switch = Hooks.builtins.update_current_buffer_on_switch
+
+    Hooks.register(Hooks.type.SWITCH, function (path, prev_path)
+      vim.notify("Moved from " .. prev_path .. " to " .. path)
+      update_on_switch(path, prev_path)
+    end)
+
+    Hooks.register(Hooks.type.DELETE, function ()
+      vim.cmd(config.update_on_change_command)
+    end)
   '';
   type = "lua";
 }
