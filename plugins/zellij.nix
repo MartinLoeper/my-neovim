d8c7619ad415ff2976b2bfbe1af7c857203c6bf1
Martin Löper <martin.loeper@nesto-software.de>
2025-03-20 18:16:08 +0100

    fix: streamline window nav

--- plugins/zellij.nix
+++ plugins/zellij.nix
@@ -3,10 +3,10 @@
   config = ''
     require("zellij-nav").setup()
 
-    vim.keymap.set("n", "<c-h>", "<cmd>ZellijNavigateLeftTab<cr>",  { desc = "navigate left or tab"  })
-    vim.keymap.set("n", "<c-j>", "<cmd>ZellijNavigateDown<cr>",  { desc = "navigate down" })
-    vim.keymap.set("n", "<c-k>", "<cmd>ZellijNavigateUp<cr>",    { desc = "navigate up" })
-    vim.keymap.set("n", "<c-l>", "<cmd>ZellijNavigateRightTab<cr>", { desc = "navigate right or tab" })
+    vim.keymap.set("n", "<c-left>", "<cmd>ZellijNavigateLeftTab<cr>",  { desc = "navigate left or tab"  })
+    vim.keymap.set("n", "<c-down>", "<cmd>ZellijNavigateDown<cr>",  { desc = "navigate down" })
+    vim.keymap.set("n", "<c-up>", "<cmd>ZellijNavigateUp<cr>",    { desc = "navigate up" })
+    vim.keymap.set("n", "<c-right>", "<cmd>ZellijNavigateRightTab<cr>", { desc = "navigate right or tab" })
   '';
   type = "lua";
 }
