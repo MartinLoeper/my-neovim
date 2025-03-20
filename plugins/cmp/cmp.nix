fa5a6e2ef8ba6763fabd69b61eb301827030b676
Martin Löper <martin.loeper@nesto-software.de>
2025-03-20 10:20:19 +0100

    debug: cmp

--- plugins/cmp/cmp.nix
+++ plugins/cmp/cmp.nix
@@ -112,9 +112,6 @@
     cmp.setup.cmdline(':', {
       mapping = cmp.mapping.preset.cmdline(),
       sources = cmp.config.sources({
-        { name = 'path' },
-        { name = 'cmdline' },
-        { name = 'cmdline_history' }
       }),
       matching = { disallow_symbol_nonprefix_matching = false }
     })
