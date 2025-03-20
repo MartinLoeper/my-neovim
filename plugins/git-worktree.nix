879965f2e1654f9daf7bac1740738d69fcf0e8a8
Martin Löper <martin.loeper@nesto-software.de>
2025-03-20 12:43:06 +0100

    fix

--- plugins/git-worktree.nix
+++ plugins/git-worktree.nix
@@ -2,7 +2,7 @@
   # WARN: primagen apparently abandoned his plugin, so we use a fork
   plugin = (pkgs.vimPlugins.git-worktree-nvim.overrideAttrs (oldAttrs: {
     src = pkgs.fetchFromGitHub {
-      owner = "natecraddock";
+      owner = "polarmutex";
       repo = "git-worktree.nvim";
       rev = "3ad8c17a3d178ac19be925284389c14114638ebb";
       sha256 = "sha256-LKC0y6/+6PWlIIWpfBdfaTFVnndFve7id2kBMwrIlKQ=";
