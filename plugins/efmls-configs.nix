144ed545e55a348e441b724db78443129d3f7bf9
Martin Löper <martin.loeper@nesto-software.de>
2025-03-21 01:49:18 +0100

    fix

--- plugins/efmls-configs.nix
+++ plugins/efmls-configs.nix
@@ -25,7 +25,6 @@
     local efmls_config = {
       filetypes = vim.tbl_keys(languages),
       settings = {
-        rootMarkers = { '.git/' },
         languages = languages,
       },
       init_options = {
