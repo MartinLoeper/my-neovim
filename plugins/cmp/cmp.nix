b41612054462281fc00a0561a500b9a93334ea5d
Martin Löper <martin.loeper@nesto-software.de>
2025-03-22 00:36:10 +0100

    fix

--- plugins/cmp/cmp.nix
+++ plugins/cmp/cmp.nix
@@ -92,10 +92,10 @@
       },
       preselect = cmp.PreselectMode.None,
       sources = cmp.config.sources({
+        { name = 'nvim_lsp_signature_help' },
         { name = 'nvim_lsp' },
         { name = 'vsnip' },
         { name = 'path' },
-        { name = 'nvim_lsp_signature_help' }
       }, {
         { name = 'buffer' },
       })
